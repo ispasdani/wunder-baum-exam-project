1e71880a96a7ea308246ce2f5be36f6afe1b4973
ispasdani <ispasdani14@gmail.com>
2021-12-20 01:07:33 +0100

    v1.0

---
+++ app.js
@@ -0,0 +1,60 @@
+const forest = document.querySelector("#forest");
+const sun = document.querySelector("#sun");
+const title = document.querySelector("#title");
+const arSection = document.querySelector(".ar-feature");
+const storySection = document.querySelector(".story");
+
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+
+  forest.style.top = value * 0.5 + "px";
+  sun.style.top = value * 0.5 + "px";
+  title.style.left = value * 1 + "px";
+});
+
+// window.onload = function () {
+//   const effect = document.querySelector(".effect");
+
+//   window.addEventListener("scroll", scrollEffectAppear);
+
+//   function scrollEffectAppear() {
+//     if (window.scrollY >= 500) {
+//       effect.style.opacity = "1";
+//       effect.style.transform = "translateX(0px)";
+//       effect.style.transition = "0.7s ease-in-out";
+//     } else {
+//       effect.style.opacity = "0";
+//       effect.style.transform = "translateX(-50px)";
+//     }
+//   }
+// };
+
+////////////////////RESPONSIVE NAVBAR///////////////////////
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".left-side");
+const navLink = document.querySelectorAll(".nav-button");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+// window.onload = function () {
+//   window.addEventListener("scroll", navOnScroll);
+
+//   function navOnScroll() {
+//     if (window.scrollY >= 5) {
+//       hamburger.classList.toggle("active");
+//       navMenu.classList.toggle("active");
+//     }
+//   }
+// };
