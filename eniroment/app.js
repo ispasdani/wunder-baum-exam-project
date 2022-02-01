4b03639ea4b46bfd0a9df5a9346132ed7eaec704
ispasdani <ispasdani14@gmail.com>
2022-02-01 18:18:29 +0100

    Update app.js

--- eniroment/app.js
+++ eniroment/app.js
@@ -6,94 +6,145 @@ import { ARButton } from "https://unpkg.com/three@0.126.0/examples/jsm/webxr/ARB
 const canvas = document.getElementById("webgl");
 const enviroment = document.getElementById("enviroment");
 
-let scene, camera, renderer;
-let obj;
-// let controls;
-let controller;
+let container;
+let camera, scene, renderer;
+let reticle;
 
 init();
 animate();
 
 function init() {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+
   scene = new THREE.Scene();
 
   camera = new THREE.PerspectiveCamera(
     70,
     window.innerWidth / window.innerHeight,
     0.01,
-    40
+    20
   );
 
-  // controls = new OrbitControls(camera, canvas);
-  // controls.update();
-
-  renderer = new THREE.WebGLRenderer({
-    alpha: true,
-    antialias: true,
-    canvas: canvas,
-  });
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.xr.enabled = true;
+  container.appendChild(renderer.domElement);
 
-  //   /////////////// this is just a test to see if everything works/////////////////
-  //   const geometry = new THREE.BoxBufferGeometry(0.3, 0.3, 0.3);
-  //   const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  //   const cube = new THREE.Mesh(geometry, material);
-  //   cube.position.set(0, 0, -1);
-  //   scene.add(cube);
-
-  const light = new THREE.AmbientLight();
+  var light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+  light.position.set(0.5, 1, 0.25);
   scene.add(light);
 
-  controller = renderer.xr.getController(0);
-  controller.addEventListener("select", onSelect);
-  scene.add(controller);
+  addReticleToScene(); // circle visual aid
+
+  const button = ARButton.createButton(renderer, {
+    requiredFeatures: ["hit-test"], // notice a new required feature
+  });
+  document.body.appendChild(button);
+  renderer.domElement.style.display = "none";
 
   enviroment.appendChild(ARButton.createButton(renderer));
   window.addEventListener("resize", onWindowResize, false);
 }
 
-function onSelect() {
-  console.log("select");
-  let loader = new GLTFLoader();
-  loader.load(
-    "../assets/3dmodel/firstPinBlue.gltf",
-    function (gltf) {
-      obj = gltf.scene;
-      obj.scale.set(0.3, 0.3, 0.3);
-      obj.position.set(0, 0, -4);
-      // .applyMatrix4(controller.matrixWorld);
-      // obj.quaternion.setFromRotationMatrix(controller.matrixWorld);
-      obj.rotation.set(20.4, 0, 0);
-      scene.add(gltf.scene);
-    },
-    undefined,
-    function (error) {
-      console.log(error);
-    }
+function addReticleToScene() {
+  const geometry = new THREE.RingBufferGeometry(0.15, 0.2, 32).rotateX(
+    -Math.PI / 2
   );
+  const material = new THREE.MeshBasicMaterial();
+
+  reticle = new THREE.Mesh(geometry, material);
+
+  // we will calculate the position and rotation of this reticle every frame manually
+  // in the render() function so matrixAutoUpdate is set to false
+  reticle.matrixAutoUpdate = false;
+  reticle.visible = false; // we start with the reticle not visible
+  scene.add(reticle);
+
+  // optional axis helper you can add to an object
+  // reticle.add(new THREE.AxesHelper(1));
 }
 
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 
-  renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
-  // controls.update();
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-// function animate() {
-//   renderer.setAnimationLoop(render);
-//   controls.update();
-// }
-
-// function render() {
-//   renderer.render(scene, camera);
-// }
-
 function animate() {
-  requestAnimationFrame(animate);
-  // controls.update();
-  renderer.render(scene, camera);
+  renderer.setAnimationLoop(render);
+}
+
+// read more about hit testing here:
+// https://github.com/immersive-web/hit-test/blob/master/hit-testing-explainer.md
+// https://web.dev/ar-hit-test/
+
+// hit testing provides the position and orientation of the intersection point, but nothing about the surfaces themselves.
+
+let hitTestSource = null;
+let localSpace = null;
+let hitTestSourceInitialized = false;
+
+// This function gets called just once to initialize a hitTestSource
+// The purpose of this function is to get a) a hit test source and b) a reference space
+async function initializeHitTestSource() {
+  const session = renderer.xr.getSession();
+
+  // Reference spaces express relationships between an origin and the world.
+
+  // For hit testing, we use the "viewer" reference space,
+  // which is based on the device's pose at the time of the hit test.
+  const viewerSpace = await session.requestReferenceSpace("viewer");
+  hitTestSource = await session.requestHitTestSource({ space: viewerSpace });
+
+  // We're going to use the reference space of "local" for drawing things.
+  // which gives us stability in terms of the environment.
+  // read more here: https://developer.mozilla.org/en-US/docs/Web/API/XRReferenceSpace
+  localSpace = await session.requestReferenceSpace("local");
+
+  // set this to true so we don't request another hit source for the rest of the session
+  hitTestSourceInitialized = true;
+
+  // In case we close the AR session by hitting the button "End AR"
+  session.addEventListener("end", () => {
+    hitTestSourceInitialized = false;
+    hitTestSource = null;
+  });
+}
+
+// the callback from 'setAnimationLoop' can also return a timestamp
+// and an XRFrame, which provides access to the information needed in
+// order to render a single frame of animation for an XRSession describing
+// a VR or AR sccene.
+function render(timestamp, frame) {
+  if (frame) {
+    // 1. create a hit test source once and keep it for all the frames
+    // this gets called only once
+    if (!hitTestSourceInitialized) {
+      initializeHitTestSource();
+    }
+
+    // 2. get hit test results
+    if (hitTestSourceInitialized) {
+      // we get the hit test results for a particular frame
+      const hitTestResults = frame.getHitTestResults(hitTestSource);
+
+      // XRHitTestResults The hit test may find multiple surfaces. The first one in the array is the one closest to the camera.
+      if (hitTestResults.length > 0) {
+        const hit = hitTestResults[0];
+        // Get a pose from the hit test result. The pose represents the pose of a point on a surface.
+        const pose = hit.getPose(localSpace);
+
+        reticle.visible = true;
+        // Transform/move the reticle image to the hit test position
+        reticle.matrix.fromArray(pose.transform.matrix);
+      } else {
+        reticle.visible = false;
+      }
+    }
+
+    renderer.render(scene, camera);
+  }
 }
