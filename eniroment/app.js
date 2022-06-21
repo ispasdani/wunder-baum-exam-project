8cc2fdffaa11d2a9f86569703270610b18692051
ispasdani <53304187+ispasdani@users.noreply.github.com>
2022-06-21 10:16:44 +0200

    scale updated

--- eniroment/app.js
+++ eniroment/app.js
@@ -76,7 +76,7 @@ function onSelect() {
       "../assets/3dmodel/firstPinBlue.gltf",
       function (gltf) {
         obj = gltf.scene;
-        obj.scale.set(0.2, 0.2, 0.2);
+        obj.scale.set(0.05, 0.05, 0.05);
         // .applyMatrix4(controller.matrixWorld);
         // obj.quaternion.setFromRotationMatrix(controller.matrixWorld);
         obj.position.setFromMatrixPosition(reticle.matrix);
